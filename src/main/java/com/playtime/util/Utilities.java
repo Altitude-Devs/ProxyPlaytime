3b52d9685666ec078f21b5f8d70d98270f654ce5
Teriuihi <teriuihi@alttd.com>
2021-06-27 21:52:34 +0200

    Removed commented out code

--- src/main/java/com/playtime/util/Utilities.java
+++ src/main/java/com/playtime/util/Utilities.java
@@ -4,10 +4,8 @@ import com.playtime.Playtime;
 import com.velocitypowered.api.proxy.Player;
 import net.luckperms.api.model.user.User;
 
-import javax.print.DocFlavor;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -81,113 +79,5 @@ public class Utilities {
     public static String capitalize(String string) {
         return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
-//    public static String convertTime(int timeInMinutes) {
-//        int days = (int) TimeUnit.MINUTES.toDays(timeInMinutes);
-//        int hours = (int) (TimeUnit.MINUTES.toHours(timeInMinutes) - TimeUnit.DAYS.toHours(days));
-//        int minutes = (int) (TimeUnit.MINUTES.toMinutes(timeInMinutes) - TimeUnit.HOURS.toMinutes(hours)
-//                - TimeUnit.DAYS.toMinutes(days));
-//
-//        String d = ChatColor.GRAY + "" + days + ChatColor.WHITE + " day";
-//        String h = ChatColor.GRAY + "" + hours + ChatColor.WHITE + " hour";
-//        String m = ChatColor.GRAY + "" + minutes + ChatColor.WHITE + " minute";
-//        String c = ChatColor.WHITE + ", ";
-//
-//        if(days == 0) {
-//            if(hours == 0) {
-//                if(minutes == 1) {
-//                    return m;
-//                } else {
-//                    return m + "s";
-//                }
-//            } else {
-//                if(hours == 1) {
-//                    if(minutes == 1) {
-//                        return h + c + m;
-//                    } else {
-//                        if(minutes == 0) {
-//                            return h;
-//                        }
-//                        return h + c + m + "s";
-//                    }
-//                } else {
-//                    if(minutes == 1) {
-//                        return h + "s" + c + m;
-//                    } else {
-//                        if (minutes == 0) {
-//                            return h + "s";
-//                        }
-//                        return h + "s" + c + m + "s";
-//                    }
-//                }
-//            }
-//        } else {
-//            if(days == 1) {
-//                if(hours == 1) {
-//                    if(minutes == 1) {
-//                        return d + c + h + c + m;
-//                    } else {
-//                        if(minutes == 0) {
-//                            return d + c + h;
-//                        }
-//                        return d + c + h + c + m + "s";
-//                    }
-//                } else {
-//                    if(minutes == 1) {
-//                        if(hours == 0) {
-//                            return d + c + m;
-//                        }
-//                        return d + c + h + "s" + c + m;
-//                    } else {
-//                        if(minutes == 0) {
-//                            if(hours == 0) {
-//                                return d;
-//                            }
-//                            return d + c + h + "s";
-//                        } else {
-//                            if(hours == 0) {
-//                                return d + c + m + "s";
-//                            }
-//                            return d + c + h + "s" + c + m + "s";
-//                        }
-//                    }
-//                }
-//            } else {
-//                if(hours == 1) {
-//                    if(minutes == 1) {
-//                        return d + "s" + c + h + c + m;
-//                    } else {
-//                        if(minutes == 0) {
-//                            return d + "s" + c + h;
-//                        }
-//                        return d + "s" + c + h + c + m + "s";
-//                    }
-//                } else {
-//                    if(minutes == 1) {
-//                        if(hours == 0) {
-//                            return d + "s" + c + m;
-//                        }
-//                        return d + "s" + c + h + "s" + c + m;
-//                    } else {
-//                        if(minutes == 0) {
-//                            if(hours == 0) {
-//                                return d + "s";
-//                            }
-//                            return d + "s" + c + h + "s";
-//                        } else {
-//                            if(hours == 0) {
-//                                return d + "s" + c + m + "s";
-//                            }
-//                            return d + "s" + c + h + "s" + c + m + "s";
-//                        }
-//
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    public static String format(String m) {
-//        return ChatColor.translateAlternateColorCodes('&', m);
-//    }
 
 }
